134b6486b9d11b9c83a1c122bae1e57815b78fea
houfio <26674115+houfio@users.noreply.github.com>
2021-02-14 14:08:12 +0100

    Add search filter

--- Moneta/ViewModels/SearchViewModel.swift
+++ Moneta/ViewModels/SearchViewModel.swift
@@ -6,9 +6,17 @@ class SearchViewModel: SearchBar {
 
     func currencies() -> [Asset] {
         state.assets.result.filter { currency in
-            !currency.fiat
+            !currency.fiat && isSearched(currency)
         }.sorted { a, b in
             a.symbol < b.symbol
         }
+    }
+
+    func isSearched(_ currency: Asset) -> Bool {
+        if text.isEmpty {
+            return true
+        }
+
+        return currency.name.localizedCaseInsensitiveContains(text) || currency.symbol.localizedCaseInsensitiveContains(text)
     }
 }
